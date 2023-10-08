33a238ca2e1b284d92a8fffb1655e1c9411906e4
Anend <anend@live.com>
2023-10-08 06:14:38 +0000

    add custom uid

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM debian:buster
 
 ENV S3FS_VERSION=1.92 S3FS_SHA256=76ebea3c0784c5c0f6b84e009d555806aff86258886ced39eee316bf02ae8750
-
+ENV UID=33
 ADD *.sh /
 
 RUN /build-s3fs.sh
--- entry.sh
+++ entry.sh
@@ -7,10 +7,11 @@ set -e
 [ "${DEBUG:-false}" == 'true' ] && { set -x; S3FS_DEBUG='-d -d'; }
 
 # Defaults
-: ${AWS_S3_AUTHFILE:='/root/.s3fs'}
+: ${AWS_S3_AUTHFILE:='/s3fs'}
 : ${AWS_S3_MOUNTPOINT:='/mnt'}
 : ${AWS_S3_URL:='https://s3.amazonaws.com'}
 : ${S3FS_ARGS:=''}
+: ${UID:='33'}
 
 # If no command specified, print error
 [ "$1" == "" ] && set -- "$@" bash -c 'echo "Error: Please specify a command to run."; exit 128'
@@ -34,6 +35,7 @@ fi
 # Write auth file if it does not exist
 if [ ! -f "${AWS_S3_AUTHFILE}" ]; then
    echo "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" > ${AWS_S3_AUTHFILE}
+   chown ${UID} ${AWS_S3_AUTHFILE}
    chmod 400 ${AWS_S3_AUTHFILE}
 fi
 
@@ -45,7 +47,7 @@ umount -f ${AWS_S3_MOUNTPOINT}|| echo "${AWS_S3_MOUNTPOINT} did not mount"
 
 
 # s3fs mount command
-s3fs $S3FS_DEBUG $S3FS_ARGS -o passwd_file=${AWS_S3_AUTHFILE} -o url=${AWS_S3_URL} ${AWS_STORAGE_BUCKET_NAME} ${AWS_S3_MOUNTPOINT}
+sudo -u#${UID} s3fs $S3FS_DEBUG $S3FS_ARGS -o passwd_file=${AWS_S3_AUTHFILE} -o url=${AWS_S3_URL} ${AWS_STORAGE_BUCKET_NAME} ${AWS_S3_MOUNTPOINT}
 
 
 
